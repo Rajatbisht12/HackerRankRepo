9402767af3f7fe043734b3a8654c35340e33a929
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2023-12-04 07:32:11 +0000

    dsa

---
+++ SolvemeFirst.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static int add(int a, int b){
+      int sum = a + b;
+       return sum;
+    }
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int result = add(a, b);
+       System.out.println(result);
+    }
+}
